a88dfed67e7f343dfe25e59d39598e36a322c71f
babi2707 <90732683+babi2707@users.noreply.github.com>
2025-09-17 16:36:05 +0100

    refactor: form creation for database addition

--- frontend/src/app/register-database/register-database.ts
+++ frontend/src/app/register-database/register-database.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ApiService } from '../api.services';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register-database',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
   templateUrl: './register-database.html',
-  styleUrls: ['./register-database.scss']
+  styleUrls: ['./register-database.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterDatabase {
+  readonly name = new FormControl('', [Validators.required]);
 
+  errorMessage = signal('');
+  hide = signal(true);
+
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  updateErrorMessage() {
+    if (this.name.hasError('required')) {
+      this.errorMessage.set('You must insert a valid name');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onRegisterNewDatabase() {}
 }
