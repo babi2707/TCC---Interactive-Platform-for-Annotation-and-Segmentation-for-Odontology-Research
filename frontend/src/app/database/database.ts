b6fdf5f434b7e346f8060ee976aaa9f6da1be13a
babi2707 <90732683+babi2707@users.noreply.github.com>
2025-09-17 13:50:27 +0100

    feature: creation of some of the elements of the database list page

--- frontend/src/app/database/database.ts
+++ frontend/src/app/database/database.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { ProfileSymbol } from "../profile-symbol/profile-symbol";
+import { ProfileSymbol } from '../profile-symbol/profile-symbol';
 
 @Component({
   selector: 'app-database',
-  imports: [ProfileSymbol],
+  imports: [
+    ProfileSymbol
+  ],
   templateUrl: './database.html',
-  styleUrl: './database.scss'
+  styleUrls: ['./database.scss'],
 })
 export class Database {
 
